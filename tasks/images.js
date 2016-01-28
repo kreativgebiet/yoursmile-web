05907319e50eaae33efb75d4354951231b3b32ee
Dominik Schmidt <visualcake@me.com>
2016-01-28 18:30:00 +0100

    add feed modal

--- tasks/images.js
+++ tasks/images.js
@@ -2,20 +2,22 @@ import gulp from 'gulp';
 import changed from 'gulp-changed';
 import imagemin from 'gulp-imagemin';
 import rev from 'gulp-rev';
+import plumber from 'gulp-plumber';
 
 import { join } from 'path';
 import browserSync from './connect';
 import { src, dest } from './config';
 
 gulp.task('images', () => {
-  gulp.src(join(src, 'images', '**/*'))
+  return gulp.src(join(src, 'images', '**/*'))
     .pipe(changed(join(dest, 'images')))
     .pipe(gulp.dest(join(dest, 'images')))
     .pipe(browserSync.stream());
 });
 
 gulp.task('images:build', () => {
-  gulp.src(join(src, 'images', '**/*'))
+  return gulp.src(join(src, 'images', '**/*'))
+    .pipe(plumber())
     .pipe(imagemin())
     .pipe(rev())
     .pipe(gulp.dest(join(dest, 'images')))
