54be772f22af5efb8e004c76905c9d50f126c71c
Dominik Schmidt <visualcake@me.com>
2016-02-06 15:21:55 +0100

    fix header on responsive screens

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,19 +1,17 @@
 import gulp from 'gulp';
+import gutil from 'gulp-util';
 import rsync from 'rsyncwrapper';
-import runSequence from 'run-sequence';
 import { dest, remote } from './config';
 
-gulp.task('deploy', () => {
-  runSequence('build', 'push:up');
-});
-
-gulp.task('push:up', done => {
+gulp.task('deploy', done => {
   rsync({
     src: dest,
     dest: `${remote.user}@${remote.host}:${remote.dest}`,
     ssh: true,
     recursive: true,
     deleteAll: true,
+    onStdout: gutil.log,
+    onStderr: gutil.log,
     exclude: ['styleguide/', '.DS_Store', '_*.html'],
   }, done);
 });
