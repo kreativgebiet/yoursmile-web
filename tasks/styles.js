e8a5c907d0b76fea4f0e4f62c751ee2088b46bb7
visualcookie <hello@visualcookie.me>
2019-04-11 11:35:33 +0200

    feat: adds login button

--- tasks/styles.js
+++ tasks/styles.js
@@ -19,7 +19,7 @@ gulp.task('styles', () => {
     .pipe(sass({
       includePaths: stylePaths(['scss', 'sass']),
     }))
-    .pipe(postcss([autoprefixer({ browsers: ['last 2 versions'] })]))
+    .pipe(postcss([autoprefixer({ browsers: ['last 4 versions'] })]))
     .pipe(gulp.dest(join(dest, 'styles')))
     .pipe(browserSync.stream());
 });
@@ -33,7 +33,7 @@ gulp.task('styles:build', () => {
     .pipe(sass({
       includePaths: stylePaths(['scss', 'sass']),
     }))
-    .pipe(postcss([autoprefixer({ browsers: ['last 2 versions'] })]))
+    .pipe(postcss([autoprefixer({ browsers: ['last 4 versions'] })]))
     .pipe(csso())
     .pipe(gulp.dest(join(dest, 'styles')));
 });
--- web.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.logger('dev'));
-app.use(express.static('dist'));
-app.listen(process.env.PORT || 5000);
